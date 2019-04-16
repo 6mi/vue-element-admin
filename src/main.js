75d238b5917b4381d7272ecc27d437f9b5ec7b23
wangjunqiao <wang371758198@163.com>
2019-04-16 20:37:33 +0800

    移除mock，把对/api的请求代理到nfine后台

--- src/main.js
+++ src/main.js
@@ -20,10 +20,10 @@ import './utils/error-log' // error log
 
 import * as filters from './filters' // global filters
 
-import { mockXHR } from '../mock' // simulation data
-
 // mock api in github pages site build
-if (process.env.NODE_ENV === 'production') { mockXHR() }
+// if (process.env.NODE_ENV === 'production') {
+
+// }
 
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium', // set element-ui default size
