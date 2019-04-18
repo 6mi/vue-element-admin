fcf1f289508c8dd9c13b48b1030f0ce2f064ab2b
wangjunqiao <wang371758198@163.com>
2019-04-18 14:10:44 +0800

    配置系统配置模块路由 以及系统设置、菜单配置的 国际语言lang配置

---
+++ src/router/modules/sys-config.js
@@ -0,0 +1,26 @@
+import Layout from '@/layout'
+
+const SysConfig = {
+  path: '/sysconfig',
+  component: Layout,
+  alwaysShow: true,
+  meta: {
+    title: 'sysConfig',
+    icon: 'system'
+  },
+  children: [
+    {
+      path: 'menu',
+      component: () => import('@/views/sys-config/menu.vue'),
+      name: 'Menu',
+      meta: { title: 'menuConfig', icon: 'menu' }
+    },
+    {
+      path: 'icon',
+      component: () => import('@/views/svg-icons/index'),
+      name: 'Icons',
+      meta: { title: 'icons', icon: 'icon' }
+    }
+  ]
+}
+export default SysConfig
---
+++ src/views/sys-config/menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>菜单列表:
+    <el-input v-model="input" placeholder="请输入内容" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  data() {
+    return {
+      input: '1213'
+    }
+  }
+}
+</script>
+
+<style>
+  el-input{
+    width: 120px;
+  }
+</style>
+
